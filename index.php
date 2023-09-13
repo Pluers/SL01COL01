cc3a2d605a4e463fff17f4866a760c76ea44b384
Rick <S1204255@student.windesheim.nl>
2023-09-13 10:16:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
-//created by me
+//Opdracht 01
+//Voorzie alle opdrachten van comments. Als iets niet lukt beschrijf wat niet gelukt is, wat heb je al geprobeerd.
+//
+//Opdracht 02
+//Maak van jou als student minimaal 10 variabele. Waarbij je probeert alle dataypes te gebruiken. Als voorbeeld (name, mobile, email, dob).
+//
+//Opdracht 03
+//Zorg dat alle variabele getoond worden op het scherm. Probeer de getoonde pagina op te maken met html.
+//
+//Opdracht 04
+//Verander met een conditional de kleur van het scherm op basis van het moment van de dag.
 
-$name = "Rick";
-$som = 1 + 2;
-?>
-<html lang="nl">
+$naam = true;
+$telefoonnummer = "2";
+$geboortedatum = "3";
+$leeftijd = "4";
+$plaats = "5";
 
-<body>
-    <?php
-    echo $name . " <br> " . $som;
-    ?>
-</body>
 
-</html>
+
+//if($naam){
+//    echo 9 % 2;
+//} else {
+//
+//}
+
+$t = "2038274983270o";
+for ($i = 0; $i < strlen($t); $i++) {
+    echo $t;
+}
